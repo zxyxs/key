2a6df38d5cf55f4024769de7b4d16a50cec3029e
zxyxs <zaixiaymj@hotmail.com>
2025-02-13 14:03:22 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('get-button').addEventListener('click', function() {
+    window.location.href = 'https://a.com';
+});
+
+document.getElementById('query-button').addEventListener('click', function() {
+    window.location.href = 'https://b.cn';
+});
